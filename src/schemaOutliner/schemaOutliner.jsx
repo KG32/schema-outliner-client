99b86eede048eeb55cee02879d4ef93a4770e6a4
KG32 <kkgroblewski@gmail.com>
2019-03-18 15:04:03 +0100

    init

---
+++ src/schemaOutliner/schemaOutliner.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class SchemaOutliner extends Component {
+  render() {
+    return (
+      <div id='schemaOutliner'>
+        <h1>Schema Outliner</h1>
+      </div>
+    )
+  }
+}
+
+
+export default SchemaOutliner;
