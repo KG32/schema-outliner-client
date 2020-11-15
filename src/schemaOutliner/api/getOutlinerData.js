3aba54d0c35f804445974fc7da67e3c1c8b5fb72
Kuba Groblewski <kkgroblewski@gmail.com>
2020-11-15 18:47:12 +0100

    uri / dbName inputs / state

--- src/schemaOutliner/api/getOutlinerData.js
+++ src/schemaOutliner/api/getOutlinerData.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-export const endpoint = 'http://localhost:8080/outline?uri=mongodb://localhost:27017&dbName=testDb&collections[]=testCollection&collections[]=testCollection2'; // DEV
-
-async function getOutlinerData(callback) {
+async function getOutlinerData(endpoint) {
   const res = await axios.get(endpoint);
   if (res.status === 200) {
     return res.data;
--- src/schemaOutliner/schemaOutliner.jsx
+++ src/schemaOutliner/schemaOutliner.jsx
@@ -1,49 +1,65 @@
 import React, { Component } from 'react';
 import './schemaOutliner.scss';
-import getOutlinerData, {endpoint} from './api/getOutlinerData';
-import CollectionOutline from './components/collectionOutline/collectionOutline'
+import getOutlinerData from './api/getOutlinerData';
+import CollectionOutline from './components/collectionOutline/collectionOutline';
 
+const epPrefix = 'http://localhost:8080'; // DEV
 
 class SchemaOutliner extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
       collections: [],
-      source: ''
+      loading: false,
+      uri: '',
+      dbName: '',
+      connected: false,
     }
   }
 
-  async componentDidMount() {
-    try {
-      const outlinerData = await getOutlinerData();
-      this.setState({ collections: outlinerData.outlinedCollections }, () => {
-        this.setState({ loading: false });
-      });
-    } catch(e) {
-      console.error(e);
-      this.setState({ loading: false });
-    }
+  async getData() {
+    this.setState({ loading: true, collections: [], connected: false }, async () => {
+      const { uri, dbName } = this.state;
+      const endpoint = `${epPrefix}/outline?uri=${uri}&dbName=${dbName}`;
 
+      try {
+        const outlinerData = await getOutlinerData(endpoint);
+        this.setState({ collections: outlinerData.outlinedCollections, }, () => {
+          this.setState({ loading: false, connected: true });
+        });
+      } catch(e) {
+        alert(e);
+        this.setState({ loading: false, connected: false });
+      }
+    });
   }
 
 
   render() {
     return (
       <div id='schemaOutliner'>
-        <h1>Schema Outliner <span style={{fontSize: '12px'}}>({endpoint})</span></h1>
+        <h1>Schema Outliner <span className='status'></span></h1>
+        <div id='controls'>
+          <input id='uriInput' value={this.state.uri} onChange={(e) => this.setState({ uri: e.target.value})} placeholder='URI' />
+          <input id='dbNameInput' value={this.state.dbName} onChange={(e) => this.setState({ dbName: e.target.value})} placeholder='database' />
+          <button onClick={()=>this.getData()}>Outline</button>
+        </div>
         {(() => {
           if(this.state.loading) {
             return <p>Loading...</p>
           } else {
-            return (
-              <div id='collectionsOutlines'>
-                {this.state.collections.map(collection => {
-                  return <CollectionOutline key={collection.name} collection={collection} />
-                })}
-              </div>
-            )
+            if (this.state.connected) {
+              return (
+                <div id='collectionsOutlines'>
+                  {this.state.collections.map(collection => {
+                    return <CollectionOutline key={collection.name} collection={collection} />
+                  })}
+                </div>
+              )
+            } else {
+              return <p>No connection.</p>
+            }
           }
         })()}
       </div>
